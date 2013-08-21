8ad96358363443e276353fda5f0473fd99a19eae
acliss19xx <acliss19xx@localhost>
2013-08-21 22:51:27 +0900

    #2 block custom template [anchor]

---
+++ acliss_faq/blocks/acliss_faq/templates/Anchor/view.php
@@ -0,0 +1,22 @@
+<?php  defined('C5_EXECUTE') or die("Access Denied.");
+?>
+
+<div class="question-answer">
+
+	<?php foreach($faqs as $faq) { ?>
+		<div class="question-title">
+		<a href="#question<?php echo $faq->afID?>">Q.<?php echo $faq->getQuestion() ?></a>
+		</div>
+	<?php } ?>
+
+	<div class="saigo">
+	<?php foreach($faqs as $faq) { ?>
+		<div id="question<?php echo $faq->afID?>" class="question">
+			Q.<?php echo $faq->getQuestion() ?>
+		</div>
+		<div  id="answer<?php echo $faq->afID?>" class ="answer">
+			A.<?php echo $faq->getAnswer() ?>
+		</div>
+	<?php } ?>
+	</div>
+</div>

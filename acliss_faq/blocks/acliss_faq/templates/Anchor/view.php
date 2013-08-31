f06796617b670435be8feb8611b61e2d521d1ac4
acliss19xx <acliss19xx@localhost>
2013-09-01 07:05:22 +0900

    Change custom template  Anchor

--- acliss_faq/blocks/acliss_faq/templates/Anchor/view.php
+++ acliss_faq/blocks/acliss_faq/templates/Anchor/view.php
@@ -2,21 +2,22 @@
 ?>
 
 <div class="question-answer">
-
-	<?php foreach ($faqs as $faq) { ?>
-		<div class="question-title">
-			<a href="#question<?php echo $faq->afID ?>">Q.<?php echo $faq->getQuestion() ?></a>
-		</div>
-	<?php } ?>
-
-	<div class="saigo">
+	<?php $th = Loader::helper('text');?>
+	<div id="question-title<?php echo $bID ?>" class="question-title">
 		<?php foreach ($faqs as $faq) { ?>
-			<div id="question<?php echo $faq->afID ?>" class="question">
-				Q.<?php echo $faq->getQuestion() ?>
-			</div>
-			<div  id="answer<?php echo $faq->afID ?>" class ="answer">
-				A.<?php echo $faq->getAnswer() ?>
+			<div class="title">
+				<a href="#question<?php echo $bID . $faq->afID; ?>">Q.<?php echo $th->sanitize($faq->getQuestion()) ?></a>
 			</div>
 		<?php } ?>
 	</div>
+
+	<?php foreach ($faqs as $faq) { ?>
+		<div id="question<?php echo $bID . $faq->afID; ?>" class="question">
+			Q.<?php echo $th->sanitize($faq->getQuestion()) ?>
+		</div>
+		<div  id="answer<?php echo $bID . $faq->afID; ?>" class ="answer">
+			A.<?php echo $faq->getAnswer() ?>
+			<div class="answer-return"><a href="#question-title<?php echo $bID ?>">一覧に戻る</a></div>
+		</div>
+	<?php } ?>
 </div>

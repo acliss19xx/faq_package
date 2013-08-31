0a78ea3c37a1a52a54fb4ca2025bd2cc3cb07f0f
acliss19xx <acliss19xx@localhost>
2013-09-01 07:05:58 +0900

    Change view files

--- acliss_faq/blocks/acliss_faq/view.php
+++ acliss_faq/blocks/acliss_faq/view.php
@@ -1,10 +1,10 @@
 <?php defined('C5_EXECUTE') or die("Access Denied.");
 ?>
-
+<?php $th = Loader::helper('text'); ?>
 <div class="question-answer">
 	<?php foreach ($faqs as $faq) { ?>
 		<div class="question">
-			Q.<?php echo $faq->getQuestion() ?>
+			Q.<?php echo $th->sanitize($faq->getQuestion()) ?>
 		</div>
 		<div  class="answer">
 			A.<?php echo $faq->getAnswer() ?>
